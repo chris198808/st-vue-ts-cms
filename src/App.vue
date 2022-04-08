ddd1ead75e226418dee1c42f2a522f923a215fad
chris198808 <821578185@qq.com>
2022-04-08 12:58:47 +0800

    feat: change config files

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,8 @@ import { defineComponent } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
+
+  
   name: 'App',
   components: {
     HelloWorld
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 1.直接通过CLI提供给我们的选项来配置
+  outputDir: './build',
+  // 2.通过configureWebpack修改webpack的配置
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src')
+      }
+    }
+  }
 })
